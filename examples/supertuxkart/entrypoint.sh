2162cd89cdebb24efac8609147869708e4c67e0e
EC2 Default User <ec2-user@ip-10-0-112-56.us-west-2.compute.internal>
2020-09-01 19:40:44 +0000

    stk

--- examples/supertuxkart/entrypoint.sh
+++ examples/supertuxkart/entrypoint.sh
@@ -17,4 +17,4 @@
 set -e
 set +x
 
-./wrapper -i "./cmake_build/bin/supertuxkart --server-config=$(pwd)/server_config.xml"
+./wrapper -i "./cmake_build/bin/supertuxkart --server-config=$(pwd)/server_config.xml --network-ai=3"
